2a806d5146b2ebd2049b341b7b729fbab1810ef0
Magno Mathias <mmathias@afilias.info>
2015-11-15 19:39:04 +0000

    Adding procfile for heroku

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ function REST() {
     self.connectMysql();
 };
 
+app.set('port', (process.env.PORT || 3000))
+
 // Add headers
 app.use(function (req, res, next) {
 
@@ -60,8 +62,8 @@ REST.prototype.configureExpress = function(connection) {
 }
 
 REST.prototype.startServer = function() {
-      app.listen(3000,function(){
-          console.log("All right ! I am alive at Port 3000.");
+      app.listen(app.get('port'),function(){
+          console.log("All right ! I am alive at Port " + app.get('port'));
       });
 }
 
