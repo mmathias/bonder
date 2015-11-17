7fd70b07074f90a5625690be16d2a821654ea9e8
Magno Mathias <mmathias@afilias.info>
2015-11-17 10:00:39 +0000

    Adding new entry points

--- rest.js
+++ rest.js
@@ -47,6 +47,18 @@ REST_ROUTER.prototype.handleRoutes = function(router, connection, md5) {
             }
         });
     });
+    router.post("/users/login",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+        var table = ["user_login","user_email", req.params.user_email, "user_password", req.params.user_password];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
     router.put("/users",function(req,res){
         var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         var table = ["user_login","user_password",md5(req.body.password),"user_email",req.body.email];
